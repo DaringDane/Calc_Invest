be7e2ff701764baafc1a040e061fe0fc713d3053
Dane Gunther <danecgunther@gmail.com>
2020-06-17 15:21:28 -0500

    Update setup.py
    Initial state of setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,25 @@
-# TODO: Fill out this file with information about your package
+import setuptools
 
-# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-# HINT: Here is an example of a setup.py file
+setuptools.setup(
+    name="calc-invest-DaringDane", # Replace with your own username
+    version="0.0.1",
+    author="Dane Gunther",
+    author_email="danecgunther@gmail.com",
+    description="Future Investment Portfolio Calculator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DaringDane/Calc_Invest",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+# reference for setup file:
 # https://packaging.python.org/tutorials/packaging-projects/
