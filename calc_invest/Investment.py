b97619b1941732ab090e5398c62ced0cefda0b1f
Dane Gunther <danecgunther@gmail.com>
2020-07-08 13:37:30 -0500

    Initial investment functionality added and user info

--- calc_invest/Investment.py
+++ calc_invest/Investment.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 class Investment_Snapshot:
     
-    def __init__(self, annual_taxable_income, current_investments, expected_pct_dividends, filing_status='single'):
+    def __init__(self, annual_taxable_income, current_investments, expected_pct_dividends, min_living_cost, filing_status='single'):
         
         """
         Overview of current investment/income situation for calculated projections of income and investment
@@ -16,12 +16,14 @@ class Investment_Snapshot:
             annual_taxable_income (float) Income of all contributing individuals before tax
             current_investments (float) total of current investments
             expected_pct_dividends (float) average expected pct yield from present and future investments (decimal)
+            min_living_cost (float) minimum annual new income user will accept living on
             filing_status (str) tax filing status (default 'single')
                 - ['single', 'mfj', 'mfs', 'hoh'] - mfj = married, filing jointly, mfs = married, filing separately, hoh = head of household
         """
         self.income = annual_taxable_income
         self.investments = current_investments
         self.dividends = expected_pct_dividends
+        self.min_living_cost = min_living_cost / 12 # monthly
         self.filing_status = filing_status
         
         
@@ -34,7 +36,7 @@ class Investment_Snapshot:
                             ):
         
         """
-        Constructs tax bracket table, sorts person into correct tax bracket, and takehome after tax
+        Constructs tax bracket table, sorts person into correct tax bracket, and monthly takehome after tax
         
         Args:
             brackets (list of floats) current tax bracket percentages
@@ -42,6 +44,8 @@ class Investment_Snapshot:
             mar_joint (list of floats) current married, filing jointly upper income thresholds for tax bracket
             mar_sep (list of floats) current married, filing separately upper income thresholds for tax bracket
             headoh (list of floats) current head of household upper income thresholds for tax bracket
+        Returns:
+            monthly net income after tax is withdrawn
         """
         ### updated with tax withholdings for 2021
         
@@ -51,13 +55,13 @@ class Investment_Snapshot:
         # return properly taxed income
         for i in range(1, len(filing)):
             if self.income <= filing[i - 1]:
-                return self.income * (1.0 - brackets[i - 1])
+                return self.income * (1.0 - brackets[i - 1]) / 12
             elif self.income <= filing[i] and self.income > filing[i - 1]:
-                return self.income * (1.0 - brackets[i])
+                return self.income * (1.0 - brackets[i]) / 12
             else:
-                return self.income * (1.0 - brackets[i + 1])
+                return self.income * (1.0 - brackets[i + 1]) / 12
         
-    def calc_discretionary_income(self, expense_table, takehome, period="monthly"):
+    def calc_discretionary_income(self, expense_table, takehome, pct_reinvest, invest_div):
         
         """
         Takes into account expenses and income after tax to calculate discretionary income
@@ -66,11 +70,13 @@ class Investment_Snapshot:
         Args:
             expense_table (pandas df) expense names and monthly spending totals
             takehome (float) net income after tax
-            period (str) "monthly" or "annual" 
+            pct_reinvest (float) decimal percent of monthly takehome to reinvest each month
+        Returns:
+            tbd
         """
-        if period == "monthly":
-            monthly_di = takehome - np.sum(expense_table[expense_table.columns[1]])
-            return monthly_di
+        total_expenses = np.sum(expense_table[expense_table.columns[1]])
+        monthly_di = takehome - total_expenses
+        return monthly_di
 
         # elif period == "annual":
         #     annual_di = takehome - np.sum
--- calc_invest/calc_invest.py
+++ calc_invest/calc_invest.py
@@ -4,21 +4,38 @@ import numpy as np
 from build_tables import build_expense_table
 from Investment import Investment_Snapshot
 
+print("This is intended to maximize your investment portfolio along with quality of life, and focuses more on your future than your present. Sacrifice in the short term will yield comfort in the long term. \n")
 
-finance_base = input("Enter annual taxable income, current investment totals, expected pct dividends from investments(decimal), and tax filing status(single = single, mfj = married, filing jointly, mfs = married, filing separately, hoh = head of households): ").split(', ')
+finance_base = input("Enter annual taxable income, current investment totals, expected pct dividends from investments(decimal), minimum net income you're able/willing to get by with, and tax filing status(single = single, mfj = married, filing jointly, mfs = married, filing separately, hoh = head of households): ").split(', ')
+print("Portfolio Started! Collecting more information...\n\n")
 
-annual_taxable_income, current_investments, expected_pct_dividends, filing_status = \
-    float(finance_base[0]), float(finance_base[1]), float(finance_base[2]), str(finance_base[3])
+bday = pd.to_datetime(input("Type in your birthday (MM/DD/YYYY): ")).date()
+today = pd.datetime.now().date()
+age = today.year - bday.year if bday.month < today.month else today.year - bday.year - 1
 
-finances = Investment_Snapshot(annual_taxable_income, current_investments, expected_pct_dividends, filing_status)
+retirement = int(input("How old would you like to be when you retire: "))
+retire_date = pd.to_datetime(f"{bday.month}, {bday.day}, {(retirement - age) + bday.year}").date()
+
+# takes to dt objects and returns the months between the dates
+def month_difference(a, b):
+    return 12 * (a.year - b.year) + (a.month - b.month)
+
+months_remaining = month_difference(retire_date, today)
+
+annual_taxable_income, current_investments, expected_pct_dividends, min_living_cost, filing_status = \
+    float(finance_base[0]), float(finance_base[1]), float(finance_base[2]), float(finance_base[3]), str(finance_base[4])
+
+finances = Investment_Snapshot(annual_taxable_income, current_investments, expected_pct_dividends, min_living_cost, filing_status)
 
 # net_takehome = finances.calc_takehome()
 expense_table = build_expense_table()
+dividends = finances.current_investments * finances.expected_pct_dividends
+finances.calc_discretionary_income(expense_table, finances.calc_takehome()/12, 0.7, dividends)
 
 
 print("\nExpected monthly takehome: {}\n".format(finances.calc_takehome()/12))
 print("Expenses: \n{} \n expense total: {}\n\n".format(expense_table.loc[:,:], np.sum(expense_table.loc[:, 'monthly_total'])))
-print("Discretionary monthly income: {}".format(finances.calc_discretionary_income(expense_table, finances.calc_takehome()/12)))
+print("Discretionary monthly income: {}".format(finances.calc_discretionary_income(expense_table, finances.calc_takehome()/12, 0.7)))
 
 # Test input in terminal:
 """
